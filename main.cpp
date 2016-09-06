21a01856d36a55fd93fdd1a4f8041e82a48035e2
yurika <yurika.miyake@ams.eng.osaka-u.ac.jp>
2016-09-06 21:11:31 +0900

    NGH Signed-off-by: yurika <yurika.miyake@ams.eng.osaka-u.ac.jp>

---
+++ main.cpp
@@ -0,0 +1,215 @@
+#pragma once
+using namespace std;
+
+#include <iostream>
+
+
+
+void gauss(double *matrix,double *B,int N);
+
+
+int main()
+{
+	int N=4;
+	int Nc=3;
+
+	double p=10;
+
+	double fx=0;
+	double *dfx=new double [N];
+	double *rfx=new double [N*N];
+
+	double L=0;
+	double *dL=new double [N];
+	double *rL=new double [N*N];
+
+	
+	double *v=new double [Nc];
+
+	double c0=0;
+	double c1=0;
+	double c2=0;
+
+	double *dc0=new double [N];
+	double *dc1=new double [N];
+	double *dc2=new double [N];
+
+	double *rc0=new double [N*N];
+	double *rc1=new double [N*N];
+	double *rc2=new double [N*N];
+
+	double *x=new double [N];
+	double *d=new double [N];
+
+	double *Nr=new double [Nc];
+	double *Nl=new double [Nc*Nc];
+
+	double Fp=0;
+	double dFp=0;
+
+	for(int i=0;i<N;i++)
+	{
+		dfx[i]=0;
+		dL[i]=0;
+		for(int j=0;j<N;j++)
+		{
+			rfx[i*N+j]=0;
+			rL[i*N+j]=0;
+			rc0[i*N+j]=0;
+			rc1[i*N+j]=0;
+			rc2[i*N+j]=0;
+		}
+		dc0[i]=0;
+		dc1[i]=0;
+		dc2[i]=0;
+
+		x[i]=0;
+		d[i]=0;
+
+	}
+	for(int i=0;i<Nc;i++)	v[i]=0;
+
+
+	double alpha=0.1;
+
+	x[0]=10;
+	x[1]=-10;
+	x[2]=10;
+	x[3]=-10;
+
+
+	double ep=1.0e-12;	
+	double E=1;
+
+	while(E>ep)
+	{
+		for(int i=0;i<N;i++)	x[i]+=alpha*d[i];
+		cout<<"x="<<x[0]<<", "<<x[1]<<", "<<x[2]<<", "<<x[3]<<endl;
+
+		fx=x[0]*x[0]+x[1]*x[1]+2*x[2]*x[2]+x[3]*x[3]-5*x[0]-5*x[1]-21*x[2]+7*x[3];
+	
+		dfx[0]=2*x[0]-5;
+		dfx[1]=2*x[1]-5;
+		dfx[2]=2*2*x[2]-21;
+		dfx[3]=2*x[3]+7;
+
+		rfx[0*N+0]=2;
+		rfx[1*N+1]=2;
+		rfx[2*N+2]=4;
+		rfx[3*N+3]=2;
+
+		c0=x[0]*x[0]+x[1]*x[1]+x[2]*x[2]+x[3]*x[3]+x[0]-x[1]+x[2]-x[3]-8;
+		c1=x[0]*x[0]+2*x[1]*x[1]+x[2]*x[2]+2*x[3]*x[3]-x[0]-x[3]-10;
+		c2=2*x[0]*x[0]+x[1]*x[1]+x[2]*x[2]+2*x[0]-x[1]-x[3]-5;
+
+		dc0[0]=2*x[0]+1;
+		dc0[1]=2*x[1]-1;
+		dc0[2]=2*x[2]+1;
+		dc0[3]=2*x[3]-1;
+
+		dc1[0]=2*x[0]-1;
+		dc1[1]=2*2*x[1];
+		dc1[2]=2*x[2];
+		dc1[3]=2*2*x[3]-1;
+
+		dc2[0]=2*2*x[0]+2;
+		dc2[1]=2*x[1]-1;
+		dc2[2]=2*x[2];
+		dc2[3]=-1;
+	
+		rc0[0*N+0]=2;
+		rc0[1*N+1]=2;
+		rc0[2*N+2]=2;
+		rc0[3*N+3]=2;
+
+		rc1[0*N+0]=2;
+		rc1[1*N+1]=4;
+		rc1[2*N+2]=2;
+		rc1[3*N+3]=4;
+
+		rc2[0*N+0]=4;
+		rc2[1*N+1]=2;
+		rc2[2*N+2]=2;
+		rc2[3*N+3]=0;
+
+		L=fx+v[0]*c0+v[1]*c1+v[2]*c2;
+
+		for(int i=0;i<N;i++)	dL[i]=dfx[i]+v[0]*dc0[i]+v[1]*dc1[i]+v[2]*dc2[i];
+
+		for(int i=0;i<N;i++)	for(int j=0;j<N;j++)	rL[i*N+j]=rfx[i*N+j]+v[0]*rc0[i*N+j]+v[1]*rc1[i*N+j]+v[2]*rc2[i*N+j];
+
+		double last_Fp=Fp;
+
+		Fp=fx;
+		if(c0>0)	Fp+=p*c0;
+		if(c1>0)	Fp+=p*c1;
+		if(c2>0)	Fp+=p*c2;
+		cout<<"Fp="<<Fp<<endl;
+
+		dFp=-1*(d[0]*rL[0*N+0]*d[0]+d[1]*rL[1*N+1]*d[1]+d[2]*rL[2*N+2]*d[2]+d[3]*rL[3*N+3]*d[3])+v[0]*c0+v[1]*c1+v[2]*c2;
+		if(c0>0)	dFp-=p*c0;
+		if(c1>0)	dFp-=p*c1;
+		if(c2>0)	dFp-=p*c2;
+
+
+		for(int i=0;i<Nc;i++)
+		{
+			Nr[i]=-1*dfx[i]-1*(rL[i*N+0]*d[0]+rL[i*N+1]*d[1]+rL[i*N+2]*d[2]+rL[i*N+3]*d[3]);
+			Nl[i*Nc+0]=dc0[i];
+			Nl[i*Nc+1]=dc1[i];
+			Nl[i*Nc+2]=dc2[i];
+		}
+
+		gauss(Nl,Nr,Nc);
+
+		for(int i=0;i<Nc;i++)
+		{
+			v[i]=Nr[i];
+		}
+		cout<<"v="<<v[0]<<", "<<v[1]<<", "<<v[2]<<endl;
+		E=fabs(last_Fp-Fp);
+		cout<<"E="<<E<<endl;
+
+	}
+
+	delete[]	dfx;
+	delete[]	rfx;
+	delete[]	dL;
+	delete[]	rL;
+	delete[]	v;
+	delete[]	dc0;
+	delete[]	dc1;
+	delete[]	dc2;
+	delete[]	rc0;
+	delete[]	rc1;
+	delete[]	rc2;
+	delete[]	x;
+	delete[]	d;
+	delete[]	Nr;
+	delete[]	Nl;
+
+}
+
+void gauss(double *matrix,double *B,int N)
+{
+	for(int k=0;k<N;k++)
+	{
+		double akk=matrix[k*N+k];
+		
+		for(int i=0;i<N;i++)
+		{
+			if(i!=k)
+			{
+				double A=matrix[i*N+k]/akk;
+				//for(int j=0;j<N;j++)
+				for(int j=k;j<N;j++)
+				{					
+					matrix[i*N+j]-=A*matrix[k*N+j];
+				}
+				B[i]-=A*B[k];
+			}
+		}
+	}
+	for(int k=0;k<N;k++) B[k]/=matrix[k*N+k];
+}
+
